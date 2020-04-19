10f4025f9d31104e9f7346e9eed5c0397409a6bf
Shao Qi Xu <shao.qi.xu@cloud-interactive.com>
2020-04-19 16:39:27 +0800

    Modify project name

--- ci-mod/asgi.py
+++ ci-mod/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mod project.
+ASGI config for ci-mod project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mod.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ci-mod.settings')
 
 application = get_asgi_application()
--- ci-mod/wsgi.py
+++ ci-mod/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mod project.
+WSGI config for ci-mod project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mod.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ci-mod.settings')
 
 application = get_wsgi_application()
